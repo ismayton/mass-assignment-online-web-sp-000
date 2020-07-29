dc38372f875d1e2c51eab423e329097b27a131d5
Ian Mayton <ian.mayton@gmail.com>
2020-07-29 14:41:08 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,6 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-    attributes.each do {|key| 
+    attributes.each do {|key, value| self.send()
 
 end
